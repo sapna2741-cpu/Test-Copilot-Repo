9cad656078fdf6a87486d069cad38a8003a8f37f
sapna2741-cpu <sapna.2741@gmail.com>
2025-12-09 16:15:01 +0530

    Add files via upload
    Sample app to test Github copilot review process

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Sapna on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
